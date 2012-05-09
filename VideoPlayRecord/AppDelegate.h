2cc81f1d583901f99831ed392c17a8b52ebe6e73
Abdul Azeem Khan <azeem@Azeem-Mac.local>
2012-05-09 12:02:08 +0500

    Initial Commit

---
+++ VideoPlayRecord/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  VideoPlayRecord
+//
+//  Created by Abdul Azeem Khan on 5/9/12.
+//  Copyright (c) 2012 DataInvent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VideoPlayRecord/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  VideoPlayRecord
+//
+//  Created by Abdul Azeem Khan on 5/9/12.
+//  Copyright (c) 2012 DataInvent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
